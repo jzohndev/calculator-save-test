b243977a2513bba28918f97e53d1f99762cbf7e2
jzohndev <jzohndev@gmail.com>
2015-11-04 00:31:59 -0600

    test application applying saves
    Test application that will ask the user to input their name and email.
    User will strike a save button that will save information to a file.
    File will be loaded on next launch to display previous input.

---
+++ MyApplication/app/src/main/java/com/github/jzohndev/myapplication/InfoCalculator.java
@@ -0,0 +1,13 @@
+package com.github.jzohndev.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoCalculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_calculator);
+    }
+}
